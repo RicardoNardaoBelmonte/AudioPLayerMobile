1e342e8a83e6cee5f8ce85430130367c89070583
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-11-05 08:59:18 -0300

    inicio do formUsuarios componente que iremos utilizar para fazer a modal de login e registro do usuario no app

---
+++ audioplayermobile/src/components/FormUsuarios.tsx
@@ -0,0 +1,14 @@
+import { IFormUsuarios } from "../interfaces/interfcaes";
+
+
+const FormUsuarios: React.FC <IFormUsuarios> = () => {
+    return(
+        <div className="fixed inset-0 bg-black/90 items-center flex flex-col justify-center z-50">
+            <div className="flex justify-center bg-white p-10 rounded-lg relative">
+
+            </div>
+        </div>
+    )
+}
+
+export default FormUsuarios;
